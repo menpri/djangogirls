fbc34cfe81299a07e678b0a66a27342023789ed0
menpri <axel.aoitori@gmail.com>
2020-10-08 23:32:09 +0900

    セキュア

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
@@ -5,6 +6,7 @@ from .models import Post
 from .forms import PostForm
 # Create your views here.
 
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html' , {'posts': posts})
